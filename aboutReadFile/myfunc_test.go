08241a5d67859e794c99d74f5888f8781974e494
Zen <zhangyiming748@gmail.com>
2021-02-27 16:39:00 +0800

    log

---
+++ aboutReadFile/myfunc_test.go
@@ -0,0 +1,13 @@
+package aboutReadFile
+
+import (
+	"log"
+	"testing"
+)
+
+func TestReadlink(t *testing.T) {
+	testf:="tf.txt"
+	for i,v:=range Readlink(testf){
+		log.Printf("第%d行的文本是%s\n",i,v)
+	}
+}

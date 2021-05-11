bbbe2490cbc459c8ac614908b28b37003f8f2d32
zen <zhangyiming748@gmail.com>
2021-05-11 15:44:21 +0800

    更新状态

---
+++ aboutCMD/CMD_test.go
@@ -0,0 +1,8 @@
+package aboutCMD
+
+import "testing"
+
+
+func TestCMD(t *testing.T) {
+	RunCmd()
+}
--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age int
+}
 func main() {
 	fmt.Println("this is a stand program")
+	defer func() {
+		if err:=recover();err!=nil{
+			fmt.Println(err)
+		}
+	}()
+	p1:=person{
+		name: "zhangyiming",
+		age:  26,
+	}
+	p2:=person{
+		name: "zhangyiming",
+		age:  26,
+	}
+	if p1==p2{
+		fmt.Println("same people")
+	}else {
+		panic("结构体不能比较")
+	}
 }

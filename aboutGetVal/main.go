c5b58c0ec0bf4acb38be3dcc09672108992b689c
zen <zhangyiming748@gmail.com>
2021-05-20 16:19:28 +0800

    exec.command不支持管道

--- aboutGetVal/main.go
+++ aboutGetVal/main.go
@@ -9,8 +9,9 @@ import (
 func main() {
 	ret := util.GetVal("target", "cmd")
 	fmt.Println(ret)
-	s:=strings.Split(ret, " ")
+	s := strings.Split(ret, " ")
 	for i, v := range s {
 		fmt.Printf("第%d段文字为%v\n", i, v)
 	}
+	util.Shell(s)
 }

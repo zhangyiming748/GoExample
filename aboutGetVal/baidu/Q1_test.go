047a7b005ba0c043c4045f332c9994272bc061bb
zen <zhangyiming748@gmail.com>
2021-05-21 10:54:36 +0800

    使用管道执行

---
+++ aboutGetVal/baidu/Q1_test.go
@@ -0,0 +1,9 @@
+package baidu
+
+import "testing"
+
+func TestQ1(t *testing.T) {
+	i:=[...]int{1,2,3}
+	target:=2
+
+}
---
+++ aboutGetVal/util/command.go
@@ -0,0 +1,45 @@
+package util
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+
+	//"strings"
+	//"v264add2webm/util"
+)
+
+//ffmpeg -f concat -safe 0 -i work.txt -c copy /Users/zen/Movies/out.avi
+///Users/zen/Github/Widget/v264add2webm/list.txt
+func Shell(s []string) error {
+	//ns:=s[1:]
+	//cmd := exec.Command(s[0],ns...)
+	cmd:=exec.Command("ffmpeg","-codecs","|","tee","ping.log")
+	fmt.Printf("生成的命令是:%s\n", cmd)
+	stdout, err := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+	if err != nil {
+		fmt.Printf("cmd.StdoutPipe产生的错误:%v", err)
+		return err
+	}
+	if err = cmd.Start(); err != nil {
+		fmt.Printf("cmd.Run产生的错误:%v", err)
+		return err
+	}
+	// 从管道中实时获取输出并打印到终端
+	for {
+		tmp := make([]byte, 1024)
+		_, err := stdout.Read(tmp)
+		//写成输出日志
+		log.Println(string(tmp))
+		if err != nil {
+			break
+		}
+	}
+	if err = cmd.Wait(); err != nil {
+		log.Println("命令执行中有错误产生", err)
+		return err
+	}
+	return nil
+}
